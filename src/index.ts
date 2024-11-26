dbe78119f296f8b2f1c84d88677966dfb54fa4f0
David Metcalfe <crdjm1@gmail.com>
2024-11-25 18:23:38 -0600

    Cleaned up initial sample code

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,29 @@
 import { parseEpub } from "epubhub";
-const fs = require("fs");
+
+import { writeFileSync } from "./utils";
 
 const input = "./input/mobydick.epub";
 const output = "./output/mobydick_updated.epub";
 
-let writeFileSync = function (path: string, buffer: any) {
-  const permission = 438;
-  let fileDescriptor: number;
-
-  try {
-    fileDescriptor = fs.openSync(path, "w", permission);
-  } catch (e) {
-    fs.chmodSync(path, permission);
-    fileDescriptor = fs.openSync(path, "w", permission);
-  }
-
-  if (fileDescriptor) {
-    fs.writeSync(fileDescriptor, buffer, 0, buffer.length, 0);
-    fs.closeSync(fileDescriptor);
-  }
-};
-
-async function readepub() {
-  const epub = await parseEpub(
-    "/Users/crdjm/Desktop/epubs/input_epubs/local/mobydick.epub",
-  );
+async function updateEpub() {
+  const epub = await parseEpub(input);
 
   const entry = epub.spine[0];
   const txt = await entry.item.getText();
   const updated = txt.replace(/\<title\>/g, "<title>INSERTED ");
 
   await entry.item.setText(updated);
+
   const buf = await epub.toBuffer();
   writeFileSync(output, buf);
 }
 
-readepub();
+updateEpub();
+
+// Possible examples...
+// Extra metadata
+// Update epub and re-save
+// Extract list of files
+// Extract list of images from OPF
+// Extract list of images used in each chapter, with their alt text
+// Update all image alt text
