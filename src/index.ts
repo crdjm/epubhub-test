0fe734d9bb72da0711312bf47245dab835861372
David Metcalfe <crdjm1@gmail.com>
2024-11-26 13:45:07 -0600

    Initial update of the README, more to come

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,21 @@
+// These are examples of how to use epubhub (https://github.com/crdjm/epubhub)
+// to view and manipulate EPUB files.
+// epubhub is work in progress, but should be ready for use. However, if you
+// find any issues, have questions or feature requests, please contact me at
+// crdjm1@gmail.com
+//
+// I am using two fairly simple epubs for testing, Moby Dick and Peter Rabbit.
+// Both are in the input folder. The tests create updated vcersions in a generated
+// output folder.
+//
+// The tests are written in TypeScript, and use the async/await syntax. A utility file,
+// utils.ts, is included to help with writing the tests.
+//
+// They are meant for illustration purposes only, and the epubhub repo should provide
+// more complete documentation (although not wrtten yet!).
+//
+//
+
 import { parseEpub } from "epubhub";
 
 import { writeFileSync } from "./utils";
